bb23908dcbf54cc32b3358701d8d25ea10815ffa
Ahmet Furkan ŞİMŞEK <asimsekfurkan@yandex.com>
2021-02-02 19:53:01 +0300

    {0} --- {1}

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,9 +11,11 @@ namespace ConsoleUI
             CarManager carManager = new CarManager(new InMemoryCarDal());
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine("Car Id : "+car.CarId +"  Brand Id : " + car.BrandId +"  Günlük Kira : " + car.DailyPrice + "  Yılı : " + car.ModelYear + "  Marka : " +car.Desctiption );
+               Console.WriteLine("Car Id : "+car.CarId +"  Brand Id : " + car.BrandId +"  Günlük Kira : " + car.DailyPrice + "  Yılı : " + car.ModelYear + "  Marka : " +car.Desctiption );
+
+               Console.WriteLine("{0} --- {1} --- {2} --- {3} --- {4} --- {5}", car.BrandId,car.CarId,car.ColorId,car.DailyPrice,car.Desctiption,car.ModelYear );
             }
             
         }
     }
-}
+}          
